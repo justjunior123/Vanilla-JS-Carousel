148db2ca6303a1530b7af39345be94331409607d
justjunior89 <bballernumber5@sbcglobal.net>
2019-02-04 00:04:29 -0800

    Finished Carousel

---
+++ js/carousel.js
@@ -0,0 +1,94 @@
+const track = document.querySelector('.carousel__track');
+console.log(track);
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNavs = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNavs.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+
+// arrange the slides next to one another
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+
+const setSlidePosition = (slide,index) => {
+  slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track,currentSlide,targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left; + ')';
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+};
+
+const updateDots = (currentDot,targetDot) => {
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+};
+
+const hideShowArrows = (slides,prevButton,nextButton,targetIndex) => {
+  if (targetIndex === 0) {
+    prevButton.classList.add('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.add('is-hidden');
+  } else {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  }
+};
+
+// when i click left move slide to the left
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNavs.querySelector('.current-slide');
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+  moveToSlide(track,currentSlide,prevSlide); // callback function
+  updateDots(currentDot,prevDot);
+  hideShowArrows(slides,prevButton,nextButton,prevIndex);
+});
+
+// when i click right move slide to the right
+
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNavs.querySelector('.current-slide');
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide);
+  // const amountToMove = nextSlide.style.left;
+  // move to the next slide
+  moveToSlide(track,currentSlide,nextSlide); // callback() function
+  // track.style.transform = 'translateX(-' + amountToMove + ')';
+  // currentSlide.classList.remove('current-slide');
+  // nextSlide.classList.add('current-slide');
+  updateDots(currentDot,nextDot);
+  hideShowArrows(slides,prevButton,nextButton,nextIndex);
+});
+
+// when i click the nav indicators, move to that slide
+
+dotsNavs.addEventListener('click', e => {
+  // what target indicator was clicked?
+  const targetDot = e.target.closest('button');
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector('.current-slide');
+  const currentDot = dotsNavs.querySelector('.current-slide');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+  console.log(targetIndex); // when we click on a dot the console displays index
+  moveToSlide(track,currentSlide,targetSlide);
+  updateDots(currentDot,targetDot);
+  hideShowArrows(slides,prevButton,nextButton,targetIndex);
+
+});
